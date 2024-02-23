1464c6d76a5f6bb2a20927f9ae0b417b71a860d3
pankajprasad30 <pankajprasad30@gmail.com>
2024-02-23 19:22:18 +0530

    User name change for amazon

--- Seneium_Programs/Login_Amazon.java
+++ Seneium_Programs/Login_Amazon.java
@@ -14,10 +14,10 @@ public class Login_Amazon {
 		driver.get("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
 		Thread.sleep(4000);
 		WebElement a1=driver.findElement(By.id("ap_email"));
-		a1.sendKeys("pankajprasad30@hotmail.com");
+		a1.sendKeys("pankajprasad4798@gmail.com");
 		a1.sendKeys(Keys.RETURN);
 		WebElement a2=driver.findElement(By.id("ap_password"));
-		a2.sendKeys("Neha_@1209");
+		a2.sendKeys("Automation@123");
 		a2.sendKeys(Keys.RETURN);
 
 	}
